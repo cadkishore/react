acbb5cd3462b96380cd9c6c9a348ccb8bee0e2f3
cadkishore <cadkishore@gmail.com>
2020-07-09 19:33:18 +0530

    Second Committ

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import { Navbar,NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
+import {DISHES} from './shared/dishes';
 
 function App() {
+  constructor(props){
+    super(props);
+
+    this.state ={
+      dishes: DISHES
+    };
+  }
+
   return (
-    <div className="App">
+    <div>
       <Navbar dark color="primary">
         <div clasName="container">
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
+      <Menu dishes={this.state.dishes}/>
       <header className="App-header">
         
       </header>
